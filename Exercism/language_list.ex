a27c43cc7d54710c281bc9d4e58e9b11baee2e37
ahmos <kamaboko_0228@icloud.com>
2024-12-19 00:35:10 +0900

    四問解いてElixirの勉強

---
+++ Exercism/language_list.ex
@@ -0,0 +1,26 @@
+defmodule LanguageList do
+  def new() do
+    []
+  end
+
+  def add(list, language) do
+    [language | list]
+  end
+
+  def remove(list) do
+    tl(list)
+  end
+
+  def first(list) do
+    hd(list)
+  end
+
+  def count(list) do
+    length(list)
+  end
+
+  def functional_list?(list) do
+    lang_elixir = "Elixir"
+    lang_elixir in list
+  end
+end
